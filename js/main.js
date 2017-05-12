2df0dadc5fc381e7c7642226108227813a8e805a
Tanya Ramirez <tcramire@gmail.com>
2017-05-12 11:38:47 -0300

    Validacion js. Resolver validacion select

---
+++ js/main.js
@@ -0,0 +1,42 @@
+// onClick
+function mostrarData(){
+
+var nombre = document.getElementById("name").value;
+var apellido = document.getElementById("lastName").value;
+var email = document.getElementById("email").value;
+var direccion = document.getElementById("address").value;
+var telefono = document.getElementById("phone").value;
+var selection = document.getElementById("selection").value;
+var checkbox = document.getElementById("checkbox").value;
+
+
+	if(nombre === "" ){
+		alert("Ingrese su nombre")
+		return false
+
+	} else if (apellido === "" ){
+		alert("Ingrese su apellido")
+		return false
+
+	} else if (email === "" ){
+		alert("Ingrese su email")
+		return false
+
+	} else if (direccion === "" ){
+		alert("Ingrese su dirección")
+		return false
+
+	} else if (telefono === "" ){
+		alert("Ingrese su teléfono")
+		return false
+
+	} else if (selection === "" ){
+		alert("Selecciona un pack")
+		return false
+		
+	} else if (!document.getElementById("checkbox").checked){
+        alert("Suscríbete, podrías ganar té gratis ;)");
+        return false;
+    } 
+
+}
